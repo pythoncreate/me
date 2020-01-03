93ee5839af6ccae40da498e4bd9de066200c6cda
Chris Stuart <crstuart@gmail.com>
2020-01-02 21:05:05 -0500

    Updates

--- precache-manifest.39e7b64adb0d2b46423773493f364245.js
+++ precache-manifest.39e7b64adb0d2b46423773493f364245.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "71c35c10eadf1202779ea4c04e6bad0b",
+    "revision": "4f414106d46dff4904ee5892185e8f95",
     "url": "/me/index.html"
   },
   {
-    "revision": "be836773faa4d5d78e4e",
+    "revision": "b338b8b51598ae5130fa",
     "url": "/me/static/css/main.34de6062.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/me/static/js/2.10476587.chunk.js"
   },
   {
-    "revision": "be836773faa4d5d78e4e",
-    "url": "/me/static/js/main.b8a0663f.chunk.js"
+    "revision": "b338b8b51598ae5130fa",
+    "url": "/me/static/js/main.0a546eb8.chunk.js"
   },
   {
     "revision": "66be97a19e235fa95a6c",
